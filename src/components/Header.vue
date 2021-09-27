8c929286be2508f59637da238119589252c62427
ilgiz <ilgizayupov7@gmail.com>
2021-09-28 00:48:50 +0500

    Попровил Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,22 +3,5 @@
     class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow"
   >
     <router-link class="navbar-brand col-md-3 col-lg-2 me-0 px-3" to="/">QUIZ BOT</router-link>
-    <button
-      class="navbar-toggler position-absolute d-md-none collapsed"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#sidebarMenu"
-      aria-controls="sidebarMenu"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <input
-      class="form-control form-control-dark w-100"
-      type="text"
-      placeholder="Search"
-      aria-label="Search"
-    />
   </header>
 </template>
